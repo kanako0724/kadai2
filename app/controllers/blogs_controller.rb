4ffb744b20c6590bd943ab73aa819ec19518b743
kanako0724 <yamada-k@nttpc.co.jp>
2018-03-15 00:29:39 +0000

    fix

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,8 +28,8 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to blogs_url, notice: 'Blog was successfully created.' }
+        format.json { render :index, status: :created, location: @blog }
       else
         format.html { render :new }
         format.json { render json: @blog.errors, status: :unprocessable_entity }
